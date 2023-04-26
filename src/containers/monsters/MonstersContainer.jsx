7ea76dfe43134beb4a421a15bf12be9e547b3ab3
InvisibleHorse <pieceofcake.okay@gmail.com>
2023-04-26 19:25:25 +0200

    Created a loadMore button

--- src/containers/monsters/MonstersContainer.jsx
+++ src/containers/monsters/MonstersContainer.jsx
@@ -1,25 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
+import { Button, Container } from '@mui/material';
+
 import Monsters from '../../components/monsters/Monsters';
 import { getMonster } from '../../store/thunks/monster';
-import { getMonsters } from '../../store/thunks/monsters';
+import { getMonsters, loadMoreThunk } from '../../store/thunks/monsters';
 
 function MonstersContainer() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const monsters = useSelector(state => state.monsters.monsters);
+
+    const defaultMonstersLength = monsters.length;
     const onClickMoreInfo = id => {
         dispatch(getMonster(id));
         navigate(`/${id}`);
     };
+    const [limit, setLimit] = useState(3);
+    const limitStep = limit + 3;
 
     useEffect(() => {
-        dispatch(getMonsters(0, 3));
+        dispatch(getMonsters(0, limit, monsters.length));
     }, []);
 
+    const loadMore = () => {
+        dispatch(loadMoreThunk(0, limitStep));
+        setLimit(limitStep);
+    };
+
     return (
-        <Monsters onClickMoreInfo={onClickMoreInfo} monsters={monsters} />
+        <Container maxWidth="md">
+            <Monsters onClickMoreInfo={onClickMoreInfo} monsters={monsters} />
+            {limit <= defaultMonstersLength
+                ? <Button onClick={loadMore}>Load more</Button>
+                : null}
+        </Container>
     );
 }
 
--- src/store/thunks/monsters.js
+++ src/store/thunks/monsters.js
@@ -1,7 +1,16 @@
 import { setIsLoadingMonsters, setMonsters } from '../actions/monsters';
 import { monsters } from '../../api/monstersApi';
 
-export const getMonsters = (page, limit) => async dispatch => {
+export const getMonsters = (page, limit, monstersList) => async dispatch => {
+    if (monstersList === 0) {
+        dispatch(setIsLoadingMonsters(true));
+        const response = await monsters.getMonsters(page, limit);
+        dispatch(setIsLoadingMonsters(true));
+        dispatch(setMonsters(response.data));
+    }
+};
+
+export const loadMoreThunk = (page, limit) => async dispatch => {
     dispatch(setIsLoadingMonsters(true));
     const response = await monsters.getMonsters(page, limit);
     dispatch(setIsLoadingMonsters(true));
